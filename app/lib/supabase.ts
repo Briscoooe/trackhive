1d1f8db35fc91a177731a7610ac004ecb8d5266c
Briscoooe <brianbriscoe06@gmail.com>
2023-07-29 19:40:47 +0100

    build

--- app/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { Database } from "../types/supabase";
-
-const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SECRET_KEY
-);
