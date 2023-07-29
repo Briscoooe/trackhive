2d384e8ffc7cb9a0db57563c5e934521e5bc748b
Briscoooe <brianbriscoe06@gmail.com>
2023-07-29 19:41:08 +0100

    build

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -12,6 +12,6 @@ export const getCookie = (name: string) => {
   }, "");
 };
 
-export const deleteCookie = (name) => {
+export const deleteCookie = (name: string) => {
   document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
 };
