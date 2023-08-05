02674a9b34b103bc9b4ee0f32e1d85e901d66bf9
Briscoooe <brianbriscoe06@gmail.com>
2023-08-05 23:20:43 +0100

    init

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,7 +4,7 @@ import { NativeLinkTag } from "~/components/NativeLinkTag";
 export default function Index() {
   return (
     <div>
-      <h1 className="mb-4 text-4xl font-bold tracking-tight text-center sm:text-left leading-none text-gray-900 md:text-4xl lg:text-5xl">
+      <h1 className="mb-6 sm:mb-4 text-4xl font-bold tracking-tight text-center sm:text-left leading-none text-gray-900 md:text-4xl lg:text-5xl">
         Keep your Spotify playlists intact
       </h1>
 
