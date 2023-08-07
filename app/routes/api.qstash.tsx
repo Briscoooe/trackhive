d9b7f7bfebe3b97084eca80791b0869841b25bc8
Briscoooe <brianbriscoe06@gmail.com>
2023-08-07 16:17:11 +0100

    heaDers

--- app/routes/api.qstash.tsx
+++ app/routes/api.qstash.tsx
@@ -1,10 +1,18 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
+import * as process from "process";
+import { Receiver } from "@upstash/qstash";
+
+const r = new Receiver({
+  currentSigningKey: process.env.QSTASH_CURRENT_SIGNING_KEY,
+  nextSigningKey: process.env.QSTASH_NEXT_SIGNING_KEY,
+});
 
 export const loader = async ({ request }: LoaderArgs) => {
   const response = new Response();
   const url = new URL(request.url);
   return json({
-    request: JSON.stringify(request),
+    body: JSON.stringify(request.body),
+    headers: JSON.stringify(Object.fromEntries(request.headers.entries())),
   });
 };
